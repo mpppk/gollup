cd6696377d3a859c64303676ccabb35813674fac
mpppk <niboshiporipori@gmail.com>
2020-04-10 22:14:33 +0900

    Remove unused codes

--- util/util.go
+++ util/util.go
@@ -2,22 +2,8 @@
 package util
 
 import (
-	"fmt"
 	"github.com/comail/colog"
-	"strconv"
 )
-
-// ConvertStringSliceToIntSlice converts string slices to int slices.
-func ConvertStringSliceToIntSlice(stringSlice []string) (intSlice []int, err error) {
-	for _, s := range stringSlice {
-		num, err := strconv.Atoi(s)
-		if err != nil {
-			return nil, fmt.Errorf("failed to convert string slice to int slice: %w", err)
-		}
-		intSlice = append(intSlice, num)
-	}
-	return
-}
 
 // InitializeLog initialize log settings
 func InitializeLog(verbose bool) {
