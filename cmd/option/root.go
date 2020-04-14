d7868d744f41b623debc68a905ecf87ceb3c4912
niboshi <niboshiporipori@gmail.com>
2020-04-14 20:37:23 +0900

    Fix ci (#9)
    * Add --entryrpoint and remove --dirs
    
    * Remove GCP Preview workflow

--- cmd/option/root.go
+++ cmd/option/root.go
@@ -1,6 +1,7 @@
 package option
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -10,13 +11,14 @@ import (
 // RootCmdConfig is config for root command
 type RootCmdConfig struct {
 	RootRawCmdConfig
-	Dirs []string
+	TargetPackage string
+	TargetMethod  string
 }
 
 // RootCmdConfig is config for root command
 type RootRawCmdConfig struct {
-	Verbose bool
-	Dirs    string
+	Verbose    bool
+	EntryPoint string
 }
 
 // NewRootCmdConfigFromViper generate config for sum command from viper
@@ -25,8 +27,26 @@ func NewRootCmdConfigFromViper() (*RootCmdConfig, error) {
 	if err := viper.Unmarshal(&rawConf); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal config from viper: %w", err)
 	}
+	pkg, method, err := parseEntryPoint(rawConf.EntryPoint)
+	if err != nil {
+		return nil, err
+	}
 	return &RootCmdConfig{
 		RootRawCmdConfig: rawConf,
-		Dirs:             strings.Split(rawConf.Dirs, ","),
+		TargetPackage:    pkg,
+		TargetMethod:     method,
 	}, nil
+}
+
+func parseEntryPoint(entryPoint string) (string, string, error) {
+	pkgAndMethod := strings.Split(entryPoint, ".")
+	l := len(pkgAndMethod)
+	switch l {
+	case 1:
+		return "main", entryPoint, nil
+	case 2:
+		return pkgAndMethod[0], pkgAndMethod[1], nil
+	default:
+		return "", "", errors.New("invalid entryPoint: " + entryPoint)
+	}
 }
